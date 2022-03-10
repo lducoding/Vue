ccf0974c03b2aab3f1d0449a02d5bb4f2cd781c7
ldu <lducoding@gmail.com>
2022-03-10 17:23:39 +0900

    this.. 학습

---
+++ 틱택토/TdComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <td @click="onClickTd">
+    {{cellData}}
+  </td>
+</template>
+
+<script>
+export default {
+  name: "TdComponent",
+  props: {
+    rowIndex: Number,
+    cellData: String,
+    cellIndex: Number,
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    onClickTd() {
+      const rootData = this.$root.$data;
+      this.$set(rootData.tableData[this.rowIndex], this.cellIndex, rootData.turn);
+      rootData.turn = rootData.turn === 'O' ? 'X' : 'O';
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 틱택토/TicTacTo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <table-component :tableData="tableData"></table-component>
+    <div>{{turn}}님의 턴입니다.</div>
+  </div>
+</template>
+
+<script>
+import TableComponent from "./TableComponent";
+export default {
+  components: {
+    TableComponent
+  },
+  data() {
+    return {
+        tableData: [
+          ['','',''],
+          ['','',''],
+          ['','',''],
+        ],
+        turn: 'O',
+    };
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style>
+  table {
+    border-collapse: collapse;
+  }
+  td {
+    border: 1px solid black;
+    width: 200px;
+    height: 200px;
+    text-align: center;
+  }
+</style>
