cf1cd62898aee4ee1e930373e5c4754fedab0371
lducoding <lducoding@gmail.com>
2022-03-12 23:13:43 +0900

    vuex

---
+++ 틱택토Vuex/TicTacTo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div>{{turn}}님의 턴입니다.</div>
+    <table-component :tableData="tableData"></table-component>
+    <div v-if="winner">{{winner}}님의 승리</div>
+  </div>
+</template>
+
+<script>
+import store from './store'
+import TableComponent from "./TableComponent";
+export default {
+  store,
+  components: {
+    TableComponent
+  },
+  data() {
+    return {
+        tableData: [
+          ['','',''],
+          ['','',''],
+          ['','',''],
+        ],
+        turn: 'O',
+      winner: '',
+    };
+  },
+  computed: {
+
+  },
+  methods: {
+    onClickTd(rowIndex, cellIndex) {
+      // const rootData = this.$root.$data;
+
+    },
+  },
+};
+</script>
+
+<style>
+  table {
+    border-collapse: collapse;
+  }
+  td {
+    border: 1px solid black;
+    width: 200px;
+    height: 200px;
+    text-align: center;
+  }
+</style>
---
+++ 틱택토Vuex/TrComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <tr>
+    <td-component v-for="(cellData, index) in rowData" :key="index" :cell-index="index"></td-component>
+  </tr>
+</template>
+
+<script>
+import TdComponent from "./TdComponent";
+export default {
+  name: "TrComponent",
+  components: {TdComponent},
+  computed: {
+    rowData() {
+      return this.$store.state.tableData[this.rowIndex];
+    }
+  },
+  props: {
+    rowIndex: Number,
+  },
+  data() {
+    return {
+      parent: '부모'
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
