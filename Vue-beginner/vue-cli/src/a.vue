e3ea3fb896be1eae30e7ae4b071fa110a7fe853e
lducoding <lducoding@gmail.com>
2022-08-20 11:32:06 +0900

    vue-cli

---
+++ Vue-beginner/vue-cli/src/a.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>header</div>
+</template>
+
+<script>
+export default {
+  name: "a.vue",
+  methods: {
+    addNum: function () {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
